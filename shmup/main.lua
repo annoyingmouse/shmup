63e8bb196fba30d505efd4c129af92c5cc68861e
Dominic Myers <annoyingmouse@gmail.com>
2025-06-29 05:51:58 +0100

    🔧 (.vscode/settings.json): add "circfill" to the list of disabled Lua diagnostics 📝 (main.lua): add new properties to the player object for jet, score, and lives ♻️ (main.lua): refactor player movement logic to include jet animation and muzzle flash ✨ (main.lua): implement drawing of jet sprite, muzzle flash, score, and lives display

--- shmup/main.lua
+++ shmup/main.lua
@@ -7,7 +7,13 @@ function _init()
     x = 64,
     y = 64,
     sprite = 16, -- 2 or 17 is the default sprite for the player
-    speed = 2
+    speed = 2,
+    muzzle = 0, -- Muzzle flash sprite
+    jet = {
+      sprite = 19, -- 19 is the jet sprite
+    },
+    score = 10000,
+    lives = 3
   }
   bullets = {}
 end
@@ -15,6 +21,10 @@ end
 function _update() -- 30 times a second
   -- Handle player movement
   player.sprite = 17 -- Reset to default sprite
+  player.jet.sprite = player.jet.sprite + 0.5
+  if player.jet.sprite > 23 then
+    player.jet.sprite = 19 -- Reset the jet sprite after it reaches a certain point
+  end
   if btn(0) then 
     player.x = player.x - player.speed 
     player.sprite = 16 -- 1 is the left-facing sprite
@@ -41,17 +51,24 @@ function _update() -- 30 times a second
       del(bullets, bullet) -- Remove the bullet if it goes off-screen
     end
   end
+  if player.muzzle > 0 then
+    player.muzzle = player.muzzle - 1 -- Decrease the muzzle flash size
+    if player.muzzle <= 0 then
+      player.muzzle = 0 -- Reset the muzzle flash size
+    end
+  end
   -- Handle shooting
   if btnp(5) then 
     -- Fire a bullet
     local bullet = {
       x = player.x-4,
-      y = player.y-8, -- Start above the player
+      y = player.y-10, -- Start above the player
       sprite = 4, -- 4 is the bullet sprite
       speed = 4
     }
     sfx(0) -- Reset the sprite to the default
     add(bullets, bullet)
+    player.muzzle = 5
   end  
   -- check boundaries
   if (player.x < 8) then player.x = 8 end
@@ -63,9 +80,23 @@ end
 function _draw()
   cls(0)
   spr(player.sprite, player.x - 8, player.y - 8)
+  spr(player.jet.sprite, player.x - 8, player.y) -- Draw the player sprite
   -- Draw bullets
   for i = 1, #bullets do
     local bullet = bullets[i]
     spr(bullet.sprite, bullet.x - 4, bullet.y - 4)
+  end
+  if player.muzzle > 0 then
+    circfill(player.x - 5, player.y - 10, player.muzzle, 7) -- Draw a circle around the player
+    circfill(player.x - 4, player.y - 10, player.muzzle, 7) -- Draw a circle around the player
+  end
+  --print("Score: " .. player.score, 8, 8, 7) -- Display the score at the top left corner
+  print("Score: " .. player.score, 40, 1, 12)
+  for i = 1, 4 do
+    if player.lives >= i then
+      spr(25, i * 9 - 8,  1) -- Draw the heart sprite
+    else
+      spr(26, i * 9 - 8,  1) -- Draw the empty heart sprite
+    end
   end
 end
